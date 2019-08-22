bc3b688aab4bc0c737e3d7c6d838fce38f09a9bc
Byeongkeun Ahn <7p54ks3@naver.com>
2019-08-22 09:57:12 +0900

    Emergency Hotfix for Broken Layout

--- src/component/note-taking/NoteDrawer.jsx
+++ src/component/note-taking/NoteDrawer.jsx
@@ -27,6 +27,7 @@ import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
 import Modal from './Modal';
 import TextField from '@material-ui/core/TextField';
+import './modal.css';
 
 import Note from './Note';
 import Reply from './Reply';
@@ -36,8 +37,6 @@ import { NoteDrawerMain } from '.';
 import { Icon } from '@material-ui/core';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 
-import './modal.css';
-
 const InnerWrap = styled.div`
   max-width: 1024px;
   margin: 0 auto;
@@ -45,9 +44,25 @@ const InnerWrap = styled.div`
 
 //const drawerWidth = 480;
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   root: {
     display: 'flex',
+  },
+  appBar: {
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    //width: `calc(100% - ${drawerWidth}px)`,
+    width: `calc(100% - 30%)`,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    //marginRight: drawerWidth,
+    marginRight: '30%'
   },
   title: {
     flexGrow: 1,
@@ -57,22 +72,29 @@ const useStyles = makeStyles((theme) => ({
   },
   drawer: {
     //width: drawerWidth,
+    width: '30%',
+    flexShrink: 0,
   },
   drawerPaper: {
     //width: drawerWidth,
+    width: '30%'
   },
   drawerHeader: {
     display: 'flex',
     alignItems: 'center',
+    padding: '0 8px',
+    ...theme.mixins.toolbar,
     justifyContent: 'flex-start',
   },
   content: {
+    flexGrow: 1,
     padding: theme.spacing(3),
     transition: theme.transitions.create('margin', {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
     }),
     //marginRight: -drawerWidth,
+    marginRight: '-30%'
   },
   contentShift: {
     transition: theme.transitions.create('margin', {
@@ -108,20 +130,14 @@ const modalStyle = {
   },
 };
 
+
 export default function PersistentDrawerRight({ page = null, title = '' }) {
   const classes = useStyles();
   const theme = useTheme();
   const [open, setOpen] = React.useState(false);
+
   const [isShowModal, setIsShowModal] = React.useState(false);
   const [note, setNote] = React.useState('');
-
-  function handleDrawerOpen() {
-    setOpen(true);
-  }
-
-  function handleDrawerClose() {
-    setOpen(false);
-  }
 
   function requestAddnote(noteValue) {
     const data = {
@@ -133,7 +149,7 @@ export default function PersistentDrawerRight({ page = null, title = '' }) {
     };
 
     axios.post('http://13.124.213.75:3000/api/comment/', {
-      headers: { 'Content-type': 'application/x-www-form-urlencoded' }, 
+      headers: { 'Content-type': 'application/x-www-form-urlencoded' },
       ...data
     });
   }
@@ -142,6 +158,14 @@ export default function PersistentDrawerRight({ page = null, title = '' }) {
     requestAddnote(noteValue);
     setNote('');
     setIsShowModal(false);
+  }
+
+  function handleDrawerOpen() {
+    setOpen(true);
+  }
+
+  function handleDrawerClose() {
+    setOpen(false);
   }
 
   return (
@@ -164,7 +188,6 @@ export default function PersistentDrawerRight({ page = null, title = '' }) {
               edge="end"
               onClick={handleDrawerOpen}
               className={clsx(open && classes.hide)}
-              style={{ marginLeft: 'auto' }}
             >
               <MenuIcon />
             </IconButton>
@@ -176,65 +199,63 @@ export default function PersistentDrawerRight({ page = null, title = '' }) {
           })}
         >
           <div className={classes.drawerHeader} />
-          <InnerWrap>{page}</InnerWrap>
+          <InnerWrap>
+            {page}
+          </InnerWrap>
         </main>
-      </div>
-      <div onClick={(event) => { event.stopPropagation(); setIsShowModal(true); }} style={fab_style.fab}>
-        <AddIcon color="default" />
-      </div>
-      <Drawer
-        className={classes.drawer}
-        variant="persistent"
-        anchor="right"
-        open={open}
-        classes={{
-          paper: classes.drawerPaper,
-        }}
-      >
-        <div className={classes.drawerHeader}>
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'rtl' ? (
-              <ChevronLeftIcon />
-            ) : (
-              <ChevronRightIcon />
-            )}
-          </IconButton>
+        <div onClick={(event) => { event.stopPropagation(); setIsShowModal(true); }} style={fab_style.fab}>
+          <AddIcon color="default" />
         </div>
-        <Divider />
-        <div style={{ overflowY: 'scroll' }}>
-          <NoteDrawerMain />
-        </div>
-      </Drawer>
-      <Modal
-        overlayClassName="NoteModalOverlay"
-        className="EventReceiptModalContent"
-        style={modalStyle}
-        isOpen={isShowModal}
-        onRequestClose={() => {setIsShowModal(false)}}
-      >
-        <div
-          style={{
-            width: '66vw',
-            height: '30vh',
-            backgroundColor: 'white',
-            padding: '16px 22px',
-            borderRadius: '10px',
-            position: 'relative'
+        <Drawer
+          className={classes.drawer}
+          variant="persistent"
+          anchor="right"
+          open={open}
+          classes={{
+            paper: classes.drawerPaper,
           }}
         >
-          <div style={{ fontWeight: 'bold', fontSize: '1.4em' }}>노트 입력</div>
-          <TextField
-            style={{ width: '100%', marginTop: '16px' }}
-            label="Note"
-            multiline
-            rows="1"
-            defaultValue="여기에 노트를 입력해주세요"
-            value={note}
-            onChange={e => setNote(e.target.value)}
-          />
-          <button onClick={() => { addNote(note) }} style={{ position: 'absolute', bottom :'16px', width: 'calc(100% - 44px)', height: '36px', backgroundColor: '#3f51b5', color: 'white', display: 'flex', justifyContent: 'center', alignItems: 'center', textAlign: 'center', fontSize: '16px', borderRadius: '28px' }}>aaaa</button>
-        </div>
-      </Modal>
+          <div className={classes.drawerHeader}>
+            <IconButton onClick={handleDrawerClose}>
+              {theme.direction === 'rtl' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+            </IconButton>
+          </div>
+          <Divider />
+          <div style={{ overflowY: 'scroll' }}>
+            <NoteDrawerMain />
+          </div>
+        </Drawer>
+        <Modal
+          overlayClassName="NoteModalOverlay"
+          className="EventReceiptModalContent"
+          style={modalStyle}
+          isOpen={isShowModal}
+          onRequestClose={() => { setIsShowModal(false) }}
+        >
+          <div
+            style={{
+              width: '66vw',
+              height: '30vh',
+              backgroundColor: 'white',
+              padding: '16px 22px',
+              borderRadius: '10px',
+              position: 'relative'
+            }}
+          >
+            <div style={{ fontWeight: 'bold', fontSize: '1.4em' }}>노트 입력</div>
+            <TextField
+              style={{ width: '100%', marginTop: '16px' }}
+              label="Note"
+              multiline
+              rows="1"
+              defaultValue="여기에 노트를 입력해주세요"
+              value={note}
+              onChange={e => setNote(e.target.value)}
+            />
+            <button onClick={() => { addNote(note) }} style={{ position: 'absolute', bottom: '16px', width: 'calc(100% - 44px)', height: '36px', backgroundColor: '#3f51b5', color: 'white', display: 'flex', justifyContent: 'center', alignItems: 'center', textAlign: 'center', fontSize: '16px', borderRadius: '28px' }}>aaaa</button>
+          </div>
+        </Modal>
+      </div>
     </>
   );
 }
